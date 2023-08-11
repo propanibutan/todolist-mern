1f108d51ffc9ba3163592bd1f1386012435b5ff9
Patrycja Zalas <minekosakura@gmail.com>
2023-08-11 11:13:11 +0200

    keys and user model+ new todo model

--- client/src/api/readTodosRequest.js
+++ client/src/api/readTodosRequest.js
@@ -1,12 +1,8 @@
-const API_URL = `http://localhost:8800`
-
-const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY5MDUzMzI5OH0.oDYCgm0gARtvfK25AeARD7IoAI7Kb0l3xfvI5YpCMpY`
-
 export default () => {
-    return fetch(`${API_URL}/todos`, {
+    return fetch(`${import.meta.env.API_URL}/todos`, {
         method: 'GET',
         headers: {
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${import.meta.env.TOKEN}`,
             "Content-Type": 'application/json'
         }
     })
