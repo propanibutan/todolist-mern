171b8e30a17515dac79c9d4cc098bfe41514ab69
Patrycja Zalas <minekosakura@gmail.com>
2023-08-11 20:58:12 +0200

    splash page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
-import { useQuery } from "react-query";
-import readTodosRequest from "./api/readTodosRequest";
-import "./App.css";
-import ClipLoader from 'react-spinners/ClipLoader'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Splash from "./pages/Splash/Splash";
+import SignUp from "./pages/SignUp/SignUp"
+import SignIn from "./pages/SignIn/SignIn"
+import Home from "./pages/Home/Home";
+import Notify from "./pages/Notify/Notify"
+import Calendar from "./pages/Calendar/Calendar"
+import Task from "./pages/Task/Task"
+import UserSettings from "./pages/UserSettings/UserSettings"
 
-function App() {
-  const { isLoading, data: todos } = useQuery('todos', readTodosRequest);
-
-
+export default function App() {
   return (
-    <div>
-      {isLoading 
-      ? <ClipLoader size={150} />
-      : (todos.map((todo) => (
-        <div key={todo._id}>
-          {todo.text}
-          {`${todo.completed}`}
-        </div>
-      )))
-      }
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="home" element={<Home />} />
+        <Route path="notifications" element={<Notify />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="task" element={<Task />} />
+        <Route path="settings" element={<UserSettings />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
