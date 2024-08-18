b06dbd526d8eb498181b2f38adbe638ec88da05f
Patrycja Zalas <minekosakura@gmail.com>
2024-08-18 09:49:04 +0200

    fixing names in folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Splash from "./pages/Splash/Splash";
+import Home from "./pages/Home/Home";
 import SignUp from "./pages/SignUp/SignUp"
 import SignIn from "./pages/SignIn/SignIn"
-import Home from "./pages/Home/Home";
+import UserDashboard from "./pages/UserDashboard/UserDashboard";
 import Notify from "./pages/Notify/Notify"
 import Calendar from "./pages/Calendar/Calendar"
 import Task from "./pages/Task/Task"
@@ -12,10 +12,10 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Splash />} />
+        <Route path="/" element={<Home />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="signin" element={<SignIn />} />
-        <Route path="home" element={<Home />} />
+        <Route path="home" element={<UserDashboard />} />
         <Route path="notifications" element={<Notify />} />
         <Route path="calendar" element={<Calendar />} />
         <Route path="task" element={<Task />} />
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,74 @@
-import { useQuery } from "react-query";
-import ClipLoader from 'react-spinners/ClipLoader'
-import readTodosRequest from "../../api/readTodosRequest";
+import { Container, Typography, Button, Box } from "@mui/material";
+import logo from "../../assets/logo.png";
+import woman from "../../assets/women.png";
+import s from "./Home.module.css";
 
 export default function Home() {
-    const { isLoading, data: todos } = useQuery('todos', readTodosRequest);
 
 
-    return (
-      <div>
-        {isLoading 
-        ? <ClipLoader size={150} />
-        : (todos.map((todo) => (
-          <div key={todo._id}>
-            {todo.text}
-            {`${todo.completed}`}
-          </div>
-        )))
-        }
+  return (
+    <Container maxWidth="sm" sx={{ display: "flex", flexDirection: "column" }}>
+      <img
+        src={logo}
+        alt="Do it app logo"
+        style={{
+          maxWidth: "10rem",
+          height: "auto",
+          marginInline: "auto",
+          zIndex: 10,
+        }}
+      />
+      <Typography
+        variant="h4"
+        align="center"
+        sx={{ zIndex: 10, fontWeight: "bold", fontSize: 25 }}
+      >
+        Your Journey Begins Now
+      </Typography>
+      <div
+        style={{
+          maxWidth: "100%",
+          marginInline: "auto",
+          alignSelf: "center",
+          display: "flex",
+        }}
+      >
+        <img
+          src={woman}
+          alt="Image of women"
+          style={{
+            maxWidth: "90%",
+            height: "auto",
+            marginInline: "auto",
+          }}
+        />
       </div>
-    );
+      <div className={s.wrapper}>
+        <div className={s.items}>
+          <Typography variant="h6" color="white" fontSize="24px">
+            Welcome
+          </Typography>
+          <Typography variant="subtitle1" color="white">
+            Sign in to your registered account
+          </Typography>
+          <Box sx={{ border: "2px solid white", width: 42, borderRadius: 5 }} />
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              borderRadius: 4,
+              bgcolor: "white",
+              color: "#3870FB",
+              fontWeight: "bold",
+              "&:hover": { bgcolor: "#2E4577", color: "white" },
+              display: "block",
+              m: "1rem auto",
+            }}
+          >
+            Get Started
+          </Button>
+        </div>
+      </div>
+    </Container>
+  );
 }
